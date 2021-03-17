60900b819954ad4e3a011e4c16c4c0cb4871cc75
wangzhiying <wangzhiying@bilibili.com>
2021-03-17 17:27:30 +0800

    在View 和context层级各增加一个自定义协程域

--- CoroutineWrapper/src/main/java/com/mistletoe/coroutinewrapper/CoroutineExtension.kt
+++ CoroutineWrapper/src/main/java/com/mistletoe/coroutinewrapper/CoroutineExtension.kt
@@ -1,8 +1,12 @@
 package com.mistletoe.coroutinewrapper
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.ContextWrapper
 import android.util.Log
+import android.view.View
 import androidx.annotation.Keep
+import androidx.annotation.MainThread
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -10,12 +14,15 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import com.mistletoe.coroutinewrapper.Config.PDispatcher
 import kotlinx.coroutines.*
+import java.lang.IllegalStateException
+import kotlin.jvm.Throws
 
 /**
  * Created by mistletoe
  * on 2020/8/27
  **/
 const val TAG = "CoroutineExtension"
+
 @Keep
 internal class JobLifecycleListener(private val job: Job) : LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
@@ -48,11 +55,14 @@ internal class JobLifecycleListener(private val job: Job) : LifecycleObserver {
  */
 @ObsoleteCoroutinesApi
 @SuppressLint("RestrictedApi")
-fun <T> LifecycleOwner.promise(p: suspend  () -> T): Pair<LifecycleOwner, Deferred<T>> {
+fun <T> LifecycleOwner.promise(p: suspend () -> T): Pair<LifecycleOwner, Deferred<T>> {
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        Log.e(TAG,"Exception in promise consumed!:${throwable.message}")
+        Log.e(TAG, "Exception in promise consumed!:${throwable.message}")
     }
-    val deferred = CoroutineScope(PDispatcher+exceptionHandler).async(context = PDispatcher, start = CoroutineStart.LAZY){
+    val deferred = CoroutineScope(PDispatcher + exceptionHandler).async(
+        context = PDispatcher,
+        start = CoroutineStart.LAZY
+    ) {
         p.invoke()
     }
     // use ArchTaskExecutor.getMainThreadExecutor() the same with paging LivePagedListBuilder create() method
@@ -87,7 +97,7 @@ infix fun <T> LifecycleOwner.promise(p: () -> Deferred<T>): Pair<LifecycleOwner,
         Log.e(TAG, "Exception in promise consumed!:${throwable.message}")
     }
     val lifecycleScope = CoroutineScope(PDispatcher + exceptionHandler)
-    val deferred = lifecycleScope.async (start = CoroutineStart.LAZY) {
+    val deferred = lifecycleScope.async(start = CoroutineStart.LAZY) {
         p.invoke().await()
     }
     val observer = lifecycleScope.coroutineContext[Job]?.let { JobLifecycleListener(it) }
@@ -110,7 +120,7 @@ infix fun <T> LifecycleOwner.promise(p: () -> Deferred<T>): Pair<LifecycleOwner,
 //协程实现js风格的then
 @SuppressLint("RestrictedApi")
 @Keep
-infix fun <T> Pair<LifecycleOwner, Deferred<T>>.then(block: (T) -> Unit):Deferred<T> {
+infix fun <T> Pair<LifecycleOwner, Deferred<T>>.then(block: (T) -> Unit): Deferred<T> {
     //这里只收集不处理
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.e(TAG, "Exception in then consumed!:${throwable.message}")
@@ -120,6 +130,7 @@ infix fun <T> Pair<LifecycleOwner, Deferred<T>>.then(block: (T) -> Unit):Deferre
     }
     return this.second
 }
+
 //协程实现js风格的catch
 @SuppressLint("RestrictedApi")
 @Keep
@@ -134,30 +145,42 @@ infix fun <T> Deferred<T>.catch(block: (throwable: Throwable) -> Unit): Deferred
     }
     return this
 }
-var globalTimeOutScope:CoroutineScope? = null
+
+var globalTimeOutScope: CoroutineScope? = null
+
 //协程实现js风格的setTimeOut
 @Keep
-fun setTimeOut(dispatcher: CoroutineDispatcher = Dispatchers.Main, time: Long, block: () -> Unit): CoroutineScope? {
+fun setTimeOut(
+    dispatcher: CoroutineDispatcher = Dispatchers.Main,
+    time: Long,
+    block: () -> Unit
+): CoroutineScope? {
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        Log.e(TAG,"Exception in setTimeOut consumed!:${throwable.message}")
+        Log.e(TAG, "Exception in setTimeOut consumed!:${throwable.message}")
     }
-    if(globalTimeOutScope ==null){
-        globalTimeOutScope = CoroutineScope(dispatcher+exceptionHandler)
+    if (globalTimeOutScope == null) {
+        globalTimeOutScope = CoroutineScope(dispatcher + exceptionHandler)
     }
     globalTimeOutScope?.launch {
         delay(time)
         block.invoke()
     }
-    return  globalTimeOutScope
+    return globalTimeOutScope
 }
-var globalIntervalScope:CoroutineScope? = null
+
+var globalIntervalScope: CoroutineScope? = null
+
 //协程实现js风格的setInterval
 @Keep
-fun setInterval(dispatcher: CoroutineDispatcher = Dispatchers.Default, time: Long, block: () -> Unit): CoroutineScope? {
+fun setInterval(
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    time: Long,
+    block: () -> Unit
+): CoroutineScope? {
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        Log.e(TAG,"Exception in setInterval consumed!:${throwable.message}")
+        Log.e(TAG, "Exception in setInterval consumed!:${throwable.message}")
     }
-    if(globalIntervalScope == null){
+    if (globalIntervalScope == null) {
         globalIntervalScope = CoroutineScope(dispatcher + exceptionHandler)
     }
     globalIntervalScope?.launch {
@@ -168,6 +191,59 @@ fun setInterval(dispatcher: CoroutineDispatcher = Dispatchers.Default, time: Lon
     }
     return globalIntervalScope
 }
+
+internal class CoroutineAttachStateChangeListener(private val job: Job?) :
+    View.OnAttachStateChangeListener {
+    override fun onViewAttachedToWindow(v: View?) {
+        //do nothing
+    }
+
+    override fun onViewDetachedFromWindow(v: View?) {
+        job?.cancel()
+        v?.removeOnAttachStateChangeListener(this)
+    }
+}
+
+//在View层级处获得一个协程域，当它从窗口移除时该协程取消
+@MainThread
+fun View.acquireObservableScope(): CoroutineScope {
+    val viewObservableScope = CoroutineScope(Dispatchers.Main.immediate + SupervisorJob())
+    val mJob = viewObservableScope.coroutineContext[Job]
+    val l = CoroutineAttachStateChangeListener(mJob)
+    addOnAttachStateChangeListener(l)
+    return viewObservableScope
+}
+
+@SuppressLint("RestrictedApi")
+@Throws(IllegalStateException::class)
+@MainThread
+fun Context.acquireLifecycleScope(): CoroutineScope {
+    lifecycle()?.let {
+        val lifecycleScope = CoroutineScope(Dispatchers.Main.immediate + SupervisorJob())
+        val mJob = lifecycleScope.coroutineContext[Job]!! // no way to be null
+        val l = JobLifecycleListener(mJob)
+        mJob.invokeOnCompletion { _ ->
+            ArchTaskExecutor.getMainThreadExecutor().execute {
+                it.removeObserver(l)
+            }
+        }
+        it.addObserver(l)
+        return lifecycleScope
+    } ?: throw IllegalStateException("error,can not get lifecycle here")
+}
+
+
+internal fun Context.lifecycle(): Lifecycle? {
+    var c = this as? ContextWrapper
+    while (c != null) {
+        if (c is LifecycleOwner) {
+            return c.lifecycle
+        }
+        c = c.baseContext as? ContextWrapper
+    }
+    return null
+}
+
 @Keep
 fun <T> doCancelableJob(task: () -> Deferred<T>) {
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
